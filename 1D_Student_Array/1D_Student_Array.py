df795b0b8222209573f55f6254d487f13f5630c3
Haleshot <hari.leo03@gmail.com>
2022-07-18 21:51:18 +0530

    Updated

--- 1D_Student_Array/1D_Student_Array.py
+++ 1D_Student_Array/1D_Student_Array.py
@@ -1,7 +1,7 @@
 # Program that compares students marks with average marks of class using 1D Array
 # Creates separate 2 lists for Students (less and greater than average marks of class)
 l1 = l2 = l3 = []
-sum = avg = 0
+sum = avg = c1 = c2 = 0
 
 n = int(input("Enter the limit for Number of Students in class : "))
 for i in range(n):
@@ -14,16 +14,19 @@ avg = sum/n
 
 for i in range(n):
     if l1[i] < avg:
+        c1 += 1
         l2.append(l1[i])
+
     elif l1[i] > avg:
+        c2 += 1
         l3.append(l1[i])
 
 print("\nThe Marks of Students less than average marks of the class is given below: \n")
-for i in range(n):
+for i in range(c1):
     print(l2[i])
 
 print("\nThe Marks of Students greater than average marks respectively is given below: \n")
-for i in range(n):
+for i in range(c2):
     print(l3[i])
 
 
