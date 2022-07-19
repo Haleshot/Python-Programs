8b9cc668831b58e4e6e895b81438844a968bf135
Haleshot <hari.leo03@gmail.com>
2022-07-19 21:20:00 +0530

    Updated

--- Array_Transpose/Array_Transpose.py
+++ Array_Transpose/Array_Transpose.py
@@ -1,5 +1,4 @@
-# Program that multiplies Matrices (NxN order/ Square matrices).
-# Multiplicaiton of Matrices
+# Program that creates Array Transpose of 2D array as entered by user.
 row = int(input("Enter the Row limit for the Matrices : "))
 column = int(input("Enter the Column limit for the Matrices : "))
 array_1 = []
@@ -30,30 +29,3 @@ for i in range(row):
         col.append(ele)
     result.append(col)
 
-
-# Multiplication of Matrices formation Formation
-while(row == column):
-    for i in range(row):
-        for j in range(row):
-            for k in range(row):
-                result[i][j] += array_1[i][k]*array_2[k][j]
-
-    break
-
-
-print("\n")
-print("The Matrix A is : \n")
-for i in array_1:
-    print(i)
-
-print("\n")
-
-print("The Matrix B is : \n")
-for i in array_2:
-    print(i)
-
-# Sum of Matrices
-print("\n")
-print("The Product of Matrices is : \n")
-for i in result:
-    print(i)
