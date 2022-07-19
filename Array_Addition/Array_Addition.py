83912d5e8a473389353b4e9871dedf47079635a6
Haleshot <hari.leo03@gmail.com>
2022-07-19 19:50:14 +0530

    Updated

--- Array_Addition/Array_Addition.py
+++ Array_Addition/Array_Addition.py
@@ -1,22 +1,16 @@
 # Program that adds two 2D matrices.
 # 2D Matrices Addition
 
-from calendar import c
-from numpy import array
-
-
-row = int(input("Enter the Row limit for the 2D Matrix : "))
-column = int(input("Enter the Column limit for the 2D Matrix : "))
-array_2d = []
+row = int(input("Enter the Row limit for the Matrix : "))
+column = int(input("Enter the Column limit for the Matrix : "))
+array = []
 
 for i in range(row):
     col = []
     for j in range(column):
-        ele = int(input("Enter the Element {} for the 2D Matrix : ".format(i)))
+        ele = int(input("Enter the Element {} for the Matrix : ".format(i)))
         col.append(ele)
-    array_2d.append(col)
+    array.append(col)
 
-for i in range(row):
-    for j in range(column):
-        print(array_2d[i][j], ", ")
-    print("\n")
+for i in array:
+    print(i)
