6a26a69527d257e618af04e274e22d04a9016d18
Haleshot <hari.leo03@gmail.com>
2022-07-20 08:48:41 +0530

    Updated

--- Binary_Search_Tree/Binary_Search_Tree.py
+++ Binary_Search_Tree/Binary_Search_Tree.py
@@ -1,10 +1,3 @@
-# C++ program to demonstrate insertion
-# in a BST recursively.
-
-from lib2to3.pytree import Node
-from logging.config import valid_ident
-from turtle import left, right
-
 # Class for defining the structure of BST which includes the root values present
 # on either side of the main root value and the value
 class BinarySearchTree:
@@ -15,14 +8,15 @@ class BinarySearchTree:
 
 def Insert_BST(root, key):
     if root is None:
-        return Node(key)
+        return BinarySearchTree(key)
     else:
-        if root.val == key:
-            print("Duplicates not allowed!\n")
+        if root.val == key or root is None:
+            return root
         elif root.val > key:
             root.left = Insert_BST(root.left, key)
         elif root.val < key:
             root.right = Insert_BST(root.right, key)
+        return root
 
 def Inorder(root):
     if root:
@@ -31,4 +25,23 @@ def Inorder(root):
         Inorder(root.right)
 
 def main():
-    
+    p = BinarySearchTree(20)
+    root = None
+    while True:
+        print("\nThe Menu is : ")
+        print("1. Insert a value in the BST\n2. Display the BST\n3. Exit\n")
+        ch = int(input("Enter the choice : "))
+
+        if ch == 1:
+            value = int(input("Enter the value to be entered into the BST : "))
+            root = Insert_BST(root, value)
+        
+        elif ch == 2:
+            Inorder(root)
+        
+        elif ch == 3:
+            exit(0)
+
+        else:
+            print("Invalid Choice!\n")
+main()
